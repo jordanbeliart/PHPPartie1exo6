ba379d99269dbaba4f80cf5c9c6a3c16d4dc48e1
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-03 09:22:09 +0100

    exo6

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$number = 140;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP Partie1 exo6</title>
+  </head>
+  <body>
+    <?php echo ($number + 30)/2; ?>
+  </body>
+</html>
